d977ad4d49fee190cf0d2b009b9d4671e693a047
karthikcrazy4 <karthikeyasiripragada4@gmail.com>
2025-12-25 16:35:15 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/login';
+import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Reports from './pages/reports';
 import Register from './pages/Register';
-import Navbar from './components/NavBar';
+import Navbar from './components/Navbar';
+
+
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
